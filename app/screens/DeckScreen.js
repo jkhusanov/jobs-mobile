c58e5343a211135aa0b7a3abac1d3b6082089727
Jakhongir Khusanov <worldj95@gmail.com>
2018-09-26 19:37:32 -0700

    Render some data such as name of job on the screen

--- app/screens/DeckScreen.js
+++ app/screens/DeckScreen.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import Swipe from '../components/Swipe';
+import { Card, Button } from 'react-native-elements';
 
 class DeckScreen extends React.Component {
+
+  renderCard(job) {
+    return (
+      <Card 
+        title={job.title}
+      >
+      <View style={styles.detailWrapper}>
+        <Text>{job.company}</Text>
+        <Text>{job.created_at}</Text>
+      </View>
+      <Text>{job.type}</Text>
+      </Card>
+    );
+  }
+
+
   render() {
     return (
 
       <View>
         {console.log(this.props.jobs)}
-        {/* <Swipe
+        <Swipe
           data={this.props.jobs}
-
-        /> */}
+          renderCard={this.renderCard}
+        />
 
       </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+  },
+})
+
 function mapStateToProps({ jobs }) {
   return { jobs: jobs };
 
